5156370b8434064aa4eaade4d1e8fb15d2814a58
Juan Manuel Mougan <juanmougan@gmail.com>
2020-03-31 22:51:58 -0300

    Full grid with no behaviour listeners

--- app/src/main/java/com/github/juanmougan/covidbattleship/MainActivity.kt
+++ app/src/main/java/com/github/juanmougan/covidbattleship/MainActivity.kt
@@ -1,17 +1,20 @@
 package com.github.juanmougan.covidbattleship
 
+import android.content.res.Resources
 import android.os.Bundle
-import android.view.ViewGroup
+import android.view.View
+import android.widget.ImageButton
 import android.widget.TableLayout
 import android.widget.TableRow
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.marginLeft
 
 class MainActivity : AppCompatActivity() {
 
     companion object {
         const val BOARD_ROW_NUMBER = 10
         const val BOARD_COL_NUMBER_PER_ROW = 10
+        const val BOARD_ID_TEMPLATE = "cell_%d_%d_btn"
     }
 
     private lateinit var playerBoard: TableLayout
@@ -19,25 +22,29 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        addListenersToAllButtons()
     }
 
-    fun fillBoardWithTheSea() {
+    fun addListenersToAllButtons() {
+        val res: Resources = resources
         playerBoard = findViewById(R.id.player_board)
         for (rows in 0 until BOARD_ROW_NUMBER) {
             val rowId = "row_$rows"
-            createBoardRow(rowId)
             for (cols in 0 until BOARD_COL_NUMBER_PER_ROW) {
-                val colId = "col_$rows$cols"
+                val cellIdentifier = String.format(BOARD_ID_TEMPLATE, rows, cols)
+                val cellId = res.getIdentifier(cellIdentifier, "id", this.packageName)
+                val cellButton = findViewById<ImageButton>(cellId)
+                addListenerToButton(cellButton)
             }
         }
     }
 
-    private fun createBoardRow(rowId: String) {
-        val row = TableRow(this)
-//        row.id = rowId
-        row.layoutParams = TableRow.LayoutParams(
-            TableRow.LayoutParams.MATCH_PARENT, 0
-        )
-        row.weightSum = 1F
+    private fun addListenerToButton(cellButton: ImageButton?) {
+        cellButton?.setOnClickListener(View.OnClickListener { b ->
+            // TODO add proper behaviour
+            Toast.makeText(this, "Clicked button with id: " + b.id, Toast.LENGTH_LONG)
+                .show()
+        })
     }
+
 }
